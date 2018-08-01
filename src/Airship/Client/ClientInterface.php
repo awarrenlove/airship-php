a77792615767f0bf86c50f838c162ea99bd6c3ef
George Li <glusa8@berkeley.edu>
2018-08-01 10:44:26 -0700

    Bump

--- src/Airship/Client/ClientInterface.php
+++ src/Airship/Client/ClientInterface.php
@@ -5,7 +5,7 @@ namespace Airship\Client;
 interface ClientInterface
 {
     const PLATFORM = 'php';
-    const VERSION = '0.2.0';
+    const VERSION = '0.2.1';
 
     const SERVER_URL = 'https://api.airshiphq.com';
     const OBJECT_GATE_VALUES_ENDPOINT = '/v1/object-gate-values/';
