34f968807004127ac27d04e0605f095b30f98b2b
Andrew Warren-Love <andrew.warrenlove@quartzy.com>
2018-09-27 13:32:10 -0400

    Allow server URL to be configured

--- src/Airship/Client/ClientInterface.php
+++ src/Airship/Client/ClientInterface.php
@@ -7,7 +7,7 @@ interface ClientInterface
     const PLATFORM = 'php';
     const VERSION = '2.0.0';
 
-    const SERVER_URL = 'http://localhost:5000';
+    const DEFAULT_SERVER_URL = 'http://localhost:5000';
     const OBJECT_GATE_VALUES_ENDPOINT = '/v2/object-values/';
 
     /**
